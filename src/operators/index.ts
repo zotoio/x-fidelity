7aeb38b7d3b288ec1b40db691a6b300dbc0bce04
wyvern8 <io@zoto.io>
2024-06-16 14:02:35 +1000

    fix

--- src/operators/index.ts
+++ src/operators/index.ts
@@ -2,12 +2,9 @@ import { RuleDefn } from '../typeDefs';
 import { currentDependencies } from './currentDependencies';
 import { fileContains } from './fileContains';
 import { directoryStructureMatches } from './directoryStructureMatches';
-import { openaiAnalysis } from './openaiAnalysis';
-
 let operators: RuleDefn[] = [];
 operators.push(currentDependencies);
 operators.push(fileContains);
 operators.push(directoryStructureMatches);
-operators.push(openaiAnalysis);
 
 export { operators };
