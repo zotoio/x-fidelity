6bab02521213667c63b28f5153905c228177e7d4
wyvern8 <io@zoto.io>
2024-07-05 21:01:08 +1000

    Update dependencies and webpack configuration for TypeScript and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        filename: 'xfidelity.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'commonjs2'
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    target: 'node'
+  entry: './src/index.ts', // Update with your entry point
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      ,
+      {
+        test: /\.json$/,
+        type: 'json',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  output: {
+    filename: 'bundle.js', // Update with your desired output filename
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
