ac16e4400197b59fa40384b3802f40dfe9bc5941
wyvern8 <io@zoto.io>
2024-08-17 19:24:29 +1000

    feat: Add telemetry reporting and improve error handling

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,66 @@
 #!/usr/bin/env node
-import { logger } from './utils/logger';
+import { generateLogPrefix, logger, setLogPrefix } from './utils/logger';
 import json from 'prettyjson';
 import { options } from "./core/cli"; 
 import { analyzeCodebase } from "./core/engine/analyzer";
 import { startServer } from './server/configServer';
+import { sendTelemetry } from './utils/telemetry';
+import { countRuleFailures, findKeyValuePair } from './utils/utils';
+
+const executionLogPrefix = generateLogPrefix();
+setLogPrefix(executionLogPrefix);
 
 logger.debug(`startup options: ${options}`);
-
+(async () => {
 try {
     if (options.mode === 'server') {
-        startServer(options.port);
+        startServer({ customPort: options.port, executionLogPrefix });
     } else {
         (async () => {
-            const results = await analyzeCodebase({
+            const resultMetadata = await analyzeCodebase({
                 repoPath: options.dir,
                 archetype: options.archetype,
                 configServer: options.configServer,
-                localConfigPath: options.localConfigPath
+                localConfigPath: options.localConfigPath,
+                executionLogPrefix
             });
 
-            // if results are found, there were warning level issues found in the codebase
-            if (results.length > 0) {
+            // if results are found, there were issues found in the codebase
+            if (resultMetadata.failureCount > 0) {
                 logger.warn('WARNING: lo-fi attributes detected in codebase!');
-                logger.warn(JSON.stringify(results));
-                console.log(`\n${json.render(results)}`);
+                logger.warn(JSON.stringify(resultMetadata.failureDetails));
+
+                if (resultMetadata.fatalityCount > 0) {
+                    logger.error(`${resultMetadata.fatalityCount} FATAL ERRORS TO BE IMMEDIATELY ADDRESSED!`);
+                }
+                console.log(`\n${json.render(resultMetadata.failureDetails)}`);
             } else {
                 // an empty array means no issues were found
                 logger.info('SUCCESS! hi-fi codebase detected.');
             }
             
         })().catch((e) => {
-            // analyzeCodebase failed which can mean a fatal issue was found in the codebase
-            logger.error('FATAL: lo-fi attributes detected in codebase!');
+            // analyzeCodebase failed 
+            logger.error('FATAL: execution failed!');
             logger.error(e.message);
             logger.error(`\n${json.render(JSON.parse(e.message))}`);
             setTimeout(() => process.exit(1), 1000);
         });
     }    
 
-} catch(e) {
-    console.error(JSON.stringify(e));
-    process.exit(1);
+} catch(e: any) {
+    await sendTelemetry({
+        eventType: 'execution failure',
+        metadata: {
+            archetype: options.archetype,
+            repoPath: options.dir,
+            ...options,
+            error: e
+        },
+        timestamp: new Date().toISOString()
+    }, executionLogPrefix);
+    logger.error(JSON.stringify(e));
+    setTimeout(() => process.exit(1), 1000);
 }
+
+})();
