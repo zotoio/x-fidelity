7f7d30e607bde3d29a68c4fcf42ecd0779029b24
wyvern8 <io@zoto.io>
2024-08-17 22:08:16 +1000

    feat: Refactor codebase analysis and reporting

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import { options } from "./core/cli";
 import { analyzeCodebase } from "./core/engine/analyzer";
 import { startServer } from './server/configServer';
 import { sendTelemetry } from './utils/telemetry';
-import { countRuleFailures } from './utils/utils';
+import { ResultMetadata } from './types/typeDefs';
 
 const executionLogPrefix = generateLogPrefix();
 setLogPrefix(executionLogPrefix);
+
+const outcomeMessage = (message: string) => `\n
+==========================================================================
+${message}
+==========================================================================`;
 
 logger.debug(`startup options: ${options}`);
 (async () => {
@@ -17,7 +22,7 @@ try {
         startServer({ customPort: options.port, executionLogPrefix });
     } else {
         (async () => {
-            const resultMetadata = await analyzeCodebase({
+            const resultMetadata: ResultMetadata = await analyzeCodebase({
                 repoPath: options.dir,
                 archetype: options.archetype,
                 configServer: options.configServer,
@@ -26,22 +31,24 @@ try {
             });
 
             // if results are found, there were issues found in the codebase
-            if (resultMetadata.failureCount > 0) {
-                logger.warn('WARNING: lo-fi attributes detected in codebase!');
-                logger.warn(JSON.stringify(resultMetadata.failureDetails));
-
-                if (resultMetadata.fatalityCount > 0) {
-                    logger.error(`${resultMetadata.fatalityCount} FATAL ERRORS TO BE IMMEDIATELY ADDRESSED!`);
+            if (resultMetadata.XFI_RESULT.totalIssues > 0) {
+                logger.warn(`WARNING: lo-fi attributes detected in codebase. ${resultMetadata.XFI_RESULT.warningCount} are warnings, ${resultMetadata.fatalityCount} are fatal.`);
+                logger.warn(JSON.stringify({XFI_RESULT: resultMetadata}));
+            
+                if (resultMetadata.XFI_RESULT.fatalityCount > 0) {
+                    logger.error(outcomeMessage(`THERE WERE ${resultMetadata.XFI_RESULT.fatalityCount} FATAL ERRORS DETECTED TO BE IMMEDIATELY ADDRESSED!`));
+                    setTimeout(() => process.exit(1), 1000);
+                } else {
+                    logger.warn(outcomeMessage('No fatal errors were found, however please review the following warnings.'));
                 }
-                console.log(`\n${json.render(resultMetadata.failureDetails)}`);
+                console.log(`\n${json.render(resultMetadata.XFI_RESULT.issueDetails)}\n\n`);
             } else {
-                // an empty array means no issues were found
-                logger.info('SUCCESS! hi-fi codebase detected.');
+                logger.info(outcomeMessage('SUCCESS! hi-fi codebase detected.'));
             }
             
         })().catch((e) => {
             // analyzeCodebase failed 
-            logger.error('FATAL: execution failed!');
+            logger.error(outcomeMessage('FATAL: execution failed!'));
             logger.error(e.message);
             logger.error(`\n${json.render(JSON.parse(e.message))}`);
             setTimeout(() => process.exit(1), 1000);
@@ -55,9 +62,10 @@ try {
             archetype: options.archetype,
             repoPath: options.dir,
             fileCount: 0,
-            failureCount: 0,
+            totalIssues: 0,
+            warningCount: 0,
             fatalityCount: 0,
-            failureDetails: [],
+            issueDetails: [],
             startTime: Date.now(),
             finishTime: Date.now(),
             durationSeconds: 0,
