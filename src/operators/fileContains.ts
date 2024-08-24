f9a8f0379fa1944b5565e2f644735aa4fc24da3c
wyvern8 <io@zoto.io>
2024-08-24 18:11:16 +1000

    fix: Update fileContains operator implementation

--- src/operators/fileContains.ts
+++ src/operators/fileContains.ts
@@ -3,24 +3,24 @@ import { OperatorDefn } from '../types/typeDefs';
 
 const fileContains: OperatorDefn = {
     'name': 'fileContains', 
-    'fn': (fileContent: any, checkString: any) => {
+    'fn': (repoFileAnalysis: any) => {
         let result = false;
-
-        const regex = new RegExp(checkString, 'g');
-        const lines = fileContent.split('\n');
-        let lineNumber = 0;
-        for (const line of lines) {
-            lineNumber++;
-            if (regex.test(line)) {
-                logger.debug(`fileContains '${checkString}' found in line ${lineNumber}: ${line}`);
-                result = true;
-                break;
-            }
-        }
         
+        try {
+            logger.debug(`fileContains: processing..`);
+
+            if (repoFileAnalysis?.result?.length > 0) {
+                return true;
+            }
+            
+        } catch (e) {
+            logger.error(`fileContains: ${e}`);
+            result = true;
+        }
+        logger.debug(`fileContains: ${result}`);
         return result;
         
-    } // oracle
+    }
 }
 
 export { fileContains };
